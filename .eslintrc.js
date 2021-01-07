e0ecace0b3b61d7fc1d1212a4041aa944b38f17e
berty <1206717557@qq.com>
2021-01-07 16:32:26 +0800

    完成登录状态

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,28 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    indent: ['off', 2],
+    'no-console': 'off',
+    'no-debugger': 'off',
+     'no-multiple-empty-lines': 0
+  },
+
+  'extends': [
+    'plugin:vue/recommended',
+    '@vue/standard'
+  ]
 }
